b223b615d8bf2b39492c360bfd4256ac3a3aa18e
Мышковский Алексей Владимирович <myshkovskii@nbrb.by>
2017-11-01 17:05:47 +0300

    added rollup config

---
+++ build/rollup.config.js
@@ -0,0 +1,78 @@
+import path from 'path'
+import babel from 'rollup-plugin-babel'
+import resolve from 'rollup-plugin-node-resolve'
+import commonjs from 'rollup-plugin-commonjs'
+import uglify from 'rollup-plugin-uglify'
+import yaml from 'rollup-plugin-yaml'
+
+const __approot =path.resolve(__dirname, '../')
+const __src = path.resolve(__approot, 'src/')
+const __dist = path.resolve(__approot, 'dist/')
+
+
+
+const bundles = []
+
+bundles.push({
+  input: path.resolve(__src, 'ndef.js'),
+  output: {
+    sourcemap: true,
+    file: path.resolve(__dist, 'index.js'),
+    format: 'cjs'
+  },
+  plugins: [
+    yaml(),
+
+    babel({
+      exclude: 'node_modules/**',
+      babelrc: false,
+      plugins: [
+        'external-helpers',
+        ['transform-object-rest-spread', { loose: true }],
+        ['check-es2015-constants', { loose: true }],
+        ['transform-es2015-arrow-functions', { loose: true }],
+        ['transform-es2015-block-scoped-functions', { loose: true }],
+        ['transform-es2015-block-scoping', { loose: true }],
+        ['transform-es2015-classes', { loose: true }],
+        ['transform-es2015-computed-properties', { loose: true }],
+        ['transform-es2015-destructuring', { loose: true }],
+        ['transform-es2015-duplicate-keys', { loose: true }],
+        ['transform-es2015-for-of', { loose: true }],
+        ['transform-es2015-function-name', { loose: true }],
+        ['transform-es2015-literals', { loose: true }],
+        ['transform-es2015-object-super', { loose: true }],
+        ['transform-es2015-parameters', { loose: true }],
+        ['transform-es2015-shorthand-properties', { loose: true }],
+        ['transform-es2015-spread', { loose: true }],
+        ['transform-es2015-sticky-regex', { loose: true }],
+        ['transform-es2015-template-literals', { loose: true }],
+        ['transform-es2015-typeof-symbol', { loose: true }],
+        ['transform-es2015-unicode-regex', { loose: true }],
+        ['transform-regenerator', { loose: true }]
+      ]
+    }),
+
+    resolve({
+      jsnext: true,
+      main: true,
+      jail: __approot,
+      preferBuiltins: false
+    }),
+
+    commonjs({
+      include: /.+/
+    })
+  ]
+})
+
+// minified
+bundles.push(Object.assign({}, bundles[0], {
+  plugins: Array.from(bundles[0]).concat([
+    uglify({
+      sourceMap: true,
+      toplevel: true
+    })
+  ])
+}))
+
+export default bundles
