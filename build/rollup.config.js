a4b0177fa3ecffb27ac728c52b3d89a2501a938a
Alexei Myshkouski <alexeimyshkouski@gmail.com>
2017-11-20 02:43:51 +0300

    cjs + esm

--- build/rollup.config.js
+++ build/rollup.config.js
@@ -3,9 +3,7 @@ import merge from 'lodash.merge'
 import babel from 'rollup-plugin-babel'
 import resolve from 'rollup-plugin-node-resolve'
 import commonjs from 'rollup-plugin-commonjs'
-import uglify from 'rollup-plugin-uglify'
 import yaml from 'rollup-plugin-yaml'
-import analyze from 'rollup-analyzer-plugin'
 
 const __approot =path.resolve(__dirname, '../')
 const __src = path.resolve(__approot, 'src/')
@@ -18,7 +16,7 @@ const bundles = []
 bundles.push({
   input: path.resolve(__src, 'ndef.js'),
   output: {
-    file: path.resolve(__dist, 'index.js'),
+    file: path.resolve(__dist, 'index.cjs.js'),
     format: 'cjs'
   },
   plugins: [
@@ -39,23 +37,11 @@ bundles.push({
   ]
 })
 
-// minified
 bundles.push(merge({}, bundles[0], {
   output: {
-    sourcemap: true,
-    file: path.resolve(__dist, 'index.min.js')
-  },
-  plugins: Array.from(bundles[0]).concat([
-    uglify({
-      sourceMap: true,
-      toplevel: true
-    }),
-
-    analyze({
-      limit: 5,
-      root: __approot
-    })
-  ])
+    file: path.resolve(__dist, 'index.esm.js'),
+    format: 'es'
+  }
 }))
 
 export default bundles
